a174df0d89beeee76d5280817f83b9c0d45addfc
Monkey King <captain.monkey88@gmail.com>
2015-07-14 08:37:44 +0300

    cookbook created by berkshelf

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'dev_java'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures dev_java'
+long_description 'Installs/Configures dev_java'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: dev_java
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
